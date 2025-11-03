0c864673ce67e830bfe10f3ac83464f146f132df
agavin132 <agavin132@gmail.com>
2025-11-03 12:16:43 -0500

    finish wifi Module

--- Wifi Module/wifimodule.h
+++ Wifi Module/wifimodule.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <Enes100.h>
 
+const char* teamName = 'Waterboys';
+byte teamType = 0;
+int markerId = 534;
+//int wifiModuleTX = ?;
+//int wifiModuleRX = ?;
+
 int main(void)
 {
     
