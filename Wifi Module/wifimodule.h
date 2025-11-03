b791554f24c425c1b15b31c34d40220ca691ffc3
agavin132 <agavin132@gmail.com>
2025-11-03 12:17:27 -0500

    integrate wifi Module

--- Wifi Module/wifimodule.h
+++ Wifi Module/wifimodule.h
@@ -1,13 +1,40 @@
-#include <stdio.h>
-#include <Enes100.h>
+#include "stdio.h"
+#include "Enes100.h"
 
-const char* teamName = 'Waterboys';
-byte teamType = 0;
-int markerId = 534;
-//int wifiModuleTX = ?;
-//int wifiModuleRX = ?;
+void setup() {
+    // Initialize Enes100 Library
+    // Team Name, Mission Type, Marker ID, Room Number, Wifi Module TX Pin, Wifi Module RX Pin
+    Enes100.begin("Waterboys", WATER, //522 or 534, 1120, //tx pin11, //rx pin8);
+    // At this point we know we are connected.
+    Enes100.println("Connected...");
+}
 
-int main(void)
-{
-    
+void loop() {
+    float x, y, t; bool v; // Declare variables to hold the data
+    //Enes100.getX will make sure you get the latest data available to you about your OTV's location.
+    //The first time getX is called, X, Y, theta and visibility are queried and cached.
+    //Subsequent calls return from the cache, so there is no performance gain to saving the function response to a variable.
+
+    x = Enes100.getX();  // Your X coordinate! 0-4, in meters, -1 if no aruco is not visibility (but you should use Enes100.isVisible to check that instead)
+    y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
+    t = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
+    v = Enes100.isVisible(); // Is your aruco visible? True or False.
+
+    if (v) // If the ArUco marker is visible
+    {
+        Enes100.print(x); // print out the location
+        Enes100.print(",");
+        Enes100.print(y);
+        Enes100.print(",");
+        Enes100.println(t);
+    }
+    else { // otherwise
+        Enes100.println("Not visible"); // print not visible
+    }
+
+    // Transmit the state of the pool
+    Enes100.mission(WATER_TYPE, FRESH_POLLUTED);
+    // Transmit the depth of the pool in mm (20, 30, or 40)
+    Enes100.mission(DEPTH, 30);
+    delay(1000);
 }
