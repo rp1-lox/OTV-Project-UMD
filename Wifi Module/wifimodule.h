0c8594dbd0cba4e5d6fd5eab6eff4e1ed4e30327
rp1-lox <duelofthefates25@gmail.com>
2025-11-05 13:40:18 -0500

    pre-integration obstacle avoidance

---
+++ Wifi Module/wifimodule.h
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "Enes100.h"
+
+void setup() {
+    // Initialize Enes100 Library
+    // Team Name, Mission Type, Marker ID, Room Number, Wifi Module TX Pin, Wifi Module RX Pin
+    int txpin = 50;
+    int rxpin = 51;
+    Enes100.begin("Waterboys", WATER, //522 or 534, 1120, txpin, rxpin);
+    // At this point we know we are connected.
+    Enes100.println("Connected...");
+}
+
+void loop() {
+    float x, y, t; bool v; // Declare variables to hold the data
+    //Enes100.getX will make sure you get the latest data available to you about your OTV's location.
+    //The first time getX is called, X, Y, theta and visibility are queried and cached.
+    //Subsequent calls return from the cache, so there is no performance gain to saving the function response to a variable.
+
+    extern x = Enes100.getX();  // Your X coordinate! 0-4, in meters, -1 if no aruco is not visibility (but you should use Enes100.isVisible to check that instead)
+    extern y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
+    extern t = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
+    extern v = Enes100.isVisible(); // Is your aruco visible? True or False.
+
+    if (v) // If the ArUco marker is visible
+    {
+        Enes100.print(x); // print out the location
+        Enes100.print(",");
+        Enes100.print(y);
+        Enes100.print(",");
+        Enes100.println(t);
+    }
+    else { // otherwise
+        Enes100.println("Not visible"); // print not visible
+    }
+
+    // Transmit the state of the pool
+    Enes100.mission(WATER_TYPE, FRESH_POLLUTED);
+    // Transmit the depth of the pool in mm (20, 30, or 40)
+    Enes100.mission(DEPTH, 30);
+    delay(1000);
+}
--- obstacleavoid.ino
+++ obstacleavoid.ino
@@ -3,31 +3,74 @@
 #include <ultrasonic.ino>
 #include <wifimodule.h>
 #include <translation.ino>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+float position == wifimodule.getY();
+int samples == 9;
+int depths[samples];
+float tolerances = 0.1; //this should be replaced by something else!!!! determine proper tolerances!!!!!!!
+float blockPos[] = {0.5, 1, 1.5};
+float score[3];
+int chosenBlock = 0;
 
 
-float x = Enes100.getX();  // Your X coordinate! 0-4, in meters, -1 if no aruco is not visibility (but you should use Enes100.isVisible to check that instead)
-float y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
-float heading = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
+void main(){
+for (int i = 0; i < samples; i++){
+    idealy = (0.25+(((i*0.5)/(samples/3)));
+    translation.loop();
+    depth[i] = ultrasonic.depth();
+}
 
+
+for(int i = 0; i < samples; i++){
+    if((i-3 <= 0) && (depth[i] <= tolerance)){
+        score[0] += 1;
+    }else if(i-6 <= 0){
+        score[1] += 1;
+    }else if (depth[i] <= tolerance){
+        score[2] += 1;
+    }
+}
+for(int i = 0; i < samples; i++){
+    if(score[i]==2){
+        chosenBlock = i;
+        break;
+    }
+}
+idealy = blockPos[chosenBlock];
+translation.loop();
+idealx = 1.9;
+translation.loop();
+}
+
+
+
+//float x = Enes100.getX();  // Your X coordinate! 0-4, in meters, -1 if no aruco is not visibility (but you should use Enes100.isVisible to check that instead)
+//float y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
+//loat heading = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
+/*
 //need distance from OS sensor and 
 
-void avoid(){
-    if (!Enes100.isVisible()) {
-        stop();
+//void avoid(){
+//    if (!Enes100.isVisible()) {
+//        stop();
 // send message that the vision system is not working 
-    }
-    else if(x < 2.8){
-        if(delta() < /*tolerence*/){
-            go();
-        }
-        else if (delta() > /*tolerence*/){
+//    }
+//    else if(x < 2.8){
+//        if(delta() < ){
+//            go(); 
+       }
+        else if (delta() > ){
             if ( y > 1){
                 right();
-            }
+          }
             else if (y < 1){
                 left();
             }
         }
     }
 }
+*/
 
